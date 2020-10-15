1d641f6e1a64a4b0a44ec23d08bc690d8dfd4106
sukeo-sukeo <syuke4522@gmail.com>
2020-10-16 05:56:50 +0900

    sessionを導入しようとするも途中

--- server.js
+++ server.js
@@ -2,12 +2,24 @@ const express = require('express')
 const app = express()
 app.disable('x-powered-by')
 
+const session = require('express-session')
 const bodyParser = require('body-parser')
 require('dotenv').config()
 
 const port = process.env.PORT || process.env.SERVER_PORT
 
 const db = require('./src/database')
+
+// app.use((req, res, next) => {
+//   if (req.session) {
+//     console.log(req.session);
+//     next();
+//   } else {
+//     console.log(req.session);
+//     next()
+//     // res.redirect('/login');
+//   }
+// });
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
